4382076162579f06576df24cab15f20b91c60864
Pradeep Sharma <pradeep.sharma.cd.mec19@itbhu.ac.in>
2021-09-23 16:47:17 +0530

    Resolving Mail Sent Error

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.post("/signup", async (req, res) => {
 					signUp_items.institute,
 				]
 			);
-			endpoint.mailer(newMember.rows[0]);
+			await endpoint.mailer(newMember.rows[0]);
 			console.log(res.headersSent);
 			// console.log("SenT!!!");
 			res.send("signedUp");
